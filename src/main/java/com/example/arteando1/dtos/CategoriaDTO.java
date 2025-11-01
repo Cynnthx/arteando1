a7df1a470602074cff5edf3da5ea2349ffecc597
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:11:04 +0100

    Creación de categoriaDTO

--- src/main/java/com/example/arteando1/dtos/CategoriaDTO.java
+++ src/main/java/com/example/arteando1/dtos/CategoriaDTO.java
@@ -1,4 +1,19 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoriaDTO {
+    private Integer id;
+    private String nombre;
+
+    public CategoriaDTO(Categoria c) {
+        this.id = c.getId();
+        this.nombre = c.getNombre();
+    }
 }
