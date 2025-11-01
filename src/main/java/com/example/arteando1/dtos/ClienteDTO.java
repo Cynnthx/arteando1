086fb9126e1935dbeef2dd5714469f77db901701
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:08:49 +0100

    Actualización ClienteDTO y CrearClienteDTO

--- src/main/java/com/example/arteando1/dtos/ClienteDTO.java
+++ src/main/java/com/example/arteando1/dtos/ClienteDTO.java
@@ -1,4 +1,37 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.Cliente;
+import com.example.arteando1.modelos.Usuario;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteDTO {
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String foto;
+    private String direccion;
+
+    // Datos del Usuario
+    private String email;
+    private String nickname;
+
+    @JsonIgnoreProperties({"cliente", "hibernateLazyInitializer", "handler"})
+    private Usuario usuario;
+
+    public ClienteDTO(Cliente c) {
+        this.id = c.getId();
+        this.nombre = c.getNombre();
+        this.apellidos = c.getApellidos();
+        this.dni = c.getDni();
+        this.foto = c.getFoto();
+        this.direccion = c.getDireccion();
+        this.usuario = c.getUsuario();
+    }
 }
--- src/main/java/com/example/arteando1/dtos/CrearClienteDTO.java
+++ src/main/java/com/example/arteando1/dtos/CrearClienteDTO.java
@@ -1,4 +1,37 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.Cliente;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CrearClienteDTO {
+
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String foto;
+    private String direccion;
+
+    // Datos del Usuario (para creación)
+    private String email;
+    private String nickname;
+    private String contrasena;
+
+    public Cliente toEntity() {
+        Cliente cliente = new Cliente();
+        cliente.setNombre(this.nombre);
+        cliente.setApellidos(this.apellidos);
+        cliente.setDni(this.dni);
+        cliente.setFoto(this.foto);
+        cliente.setDireccion(this.direccion);
+        return cliente;
+    }
+
+
 }
