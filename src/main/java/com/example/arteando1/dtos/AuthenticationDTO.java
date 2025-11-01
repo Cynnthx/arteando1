196d5d99a10f676aa764bccd2f56e492602bcb93
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:14:35 +0100

    Actualización AuthenticationDTO

--- src/main/java/com/example/arteando1/dtos/AuthenticationDTO.java
+++ src/main/java/com/example/arteando1/dtos/AuthenticationDTO.java
@@ -1,4 +1,15 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AuthenticationDTO {
+    private String token;
+    private String mensaje;
 }
