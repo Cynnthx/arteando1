9b86fd0b741ddbdd4dd91dbb743f80f1f3fd584f
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:08:15 +0100

    Creación de ImagenDTO

---
+++ src/main/java/com/example/arteando1/dtos/ImagenDTO.java
@@ -0,0 +1,12 @@
+package com.example.arteando1.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImagenDTO {
+    private String imagen;
+}
