609e42c5f6a030cb2366c0cf148312abcd5f930f
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:20:48 +0100

    Actualización UsuarioDTO

--- src/main/java/com/example/arteando1/dtos/UsuarioDTO.java
+++ src/main/java/com/example/arteando1/dtos/UsuarioDTO.java
@@ -1,4 +1,17 @@
 package com.example.arteando1.dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
+    private Integer id;
+    private String email;
+    private String nombreUsuario;
+    private String rol;
+    private String contrasena;
 }
