256bacd06362e781c8f9e160f54b22a1029c5e35
cintia <ccastelgonzalez@safareyes.es>
2025-11-01 20:09:06 +0100

    Actualización repositorio de cliente

--- src/main/java/com/example/arteando1/repositorios/ClienteRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/ClienteRepositorio.java
@@ -1,4 +1,15 @@
 package com.example.arteando1.repositorios;
 
-public interface ClienteRepositorio {
+import com.example.arteando1.modelos.Cliente;
+import com.example.arteando1.modelos.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepositorio extends JpaRepository<Cliente, Integer> {
+    Optional<Cliente> findByUsuarioId(Integer usuarioId);
+
+
 }
