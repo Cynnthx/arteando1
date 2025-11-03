596bcc831e323d9b1a00b7055052888c8ab09f12
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 20:47:37 +0100

    ♻️ Actualización de Test DTO

--- src/main/java/com/example/arteando1/dtos/TestDTO.java
+++ src/main/java/com/example/arteando1/dtos/TestDTO.java
@@ -1,4 +1,30 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.Test;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestDTO {
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private String dificultad;
+    private String categoriaNombre; // para devolver el nombre
+
+    public TestDTO(Test t) {
+        if (t == null) return;
+
+        this.id = t.getId();
+        this.titulo = t.getTitulo();
+        this.descripcion = t.getDescripcion();
+        this.dificultad = t.getDificultad();
+
+        if (t.getCategoria() != null) {
+            this.categoriaNombre = t.getCategoria().getNombre();
+        }
+    }
 }
