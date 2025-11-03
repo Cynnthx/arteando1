2f245f5e1fd4f91dd1098973b533a6bcf120f662
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 12:05:16 +0100

    Actualización UsuarioRepositorio

--- src/main/java/com/example/arteando1/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/UsuarioRepositorio.java
@@ -2,9 +2,11 @@ package com.example.arteando1.repositorios;
 
 import com.example.arteando1.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
     Usuario findByEmail(String email);
 
