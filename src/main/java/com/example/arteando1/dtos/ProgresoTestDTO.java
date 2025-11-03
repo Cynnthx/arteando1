79af372730ff707b4bae95c3885d83e6b6445c5e
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 21:08:54 +0100

    ♻️ Actualización del DTO ProgresoTest

--- src/main/java/com/example/arteando1/dtos/ProgresoTestDTO.java
+++ src/main/java/com/example/arteando1/dtos/ProgresoTestDTO.java
@@ -1,4 +1,32 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.ProgresoTest;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProgresoTestDTO {
+    private Integer id;
+    private Integer porcentajeCompletado;
+    private Integer usuarioId;
+    private String nombreUsuario;
+    private Integer testId;
+    private String tituloTest;
+
+    public ProgresoTestDTO(ProgresoTest p) {
+        if (p == null) return;
+        this.id = p.getId();
+        this.porcentajeCompletado = p.getPorcentajeCompletado();
+        if (p.getUsuario() != null) {
+            this.usuarioId = p.getUsuario().getId();
+            this.nombreUsuario = p.getUsuario().getNombreUsuario();
+        }
+        if (p.getTest() != null) {
+            this.testId = p.getTest().getId();
+            this.tituloTest = p.getTest().getTitulo();
+        }
+    }
 }
