3ae8aab90a9564f8e799fe2e22143c8fe33cbee0
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 20:47:23 +0100

    ♻️ Actualización de TestCrear DTO

--- src/main/java/com/example/arteando1/dtos/TestCrearDTO.java
+++ src/main/java/com/example/arteando1/dtos/TestCrearDTO.java
@@ -1,4 +1,15 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestCrearDTO {
+    private String titulo;
+    private String descripcion;
+    private String dificultad;
+    private Integer categoriaId;
 }
