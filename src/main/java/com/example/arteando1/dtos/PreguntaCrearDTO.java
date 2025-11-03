e4dfad0e90675c294309b7756391fe3c0a5c5073
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 20:58:41 +0100

    ♻️ Actualización del DTO CrearPregunta

--- src/main/java/com/example/arteando1/dtos/PreguntaCrearDTO.java
+++ src/main/java/com/example/arteando1/dtos/PreguntaCrearDTO.java
@@ -1,4 +1,14 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PreguntaCrearDTO {
+    private String texto;
+    private String imagen;
+    private Integer testId;
 }
