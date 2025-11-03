32877c751761d1d11cc529bb87dfe05a2ce003a1
cintia <ccastelgonzalez@safareyes.es>
2025-11-03 21:09:13 +0100

    ♻️ Actualización del DTO Crear ProgresoTest

--- src/main/java/com/example/arteando1/dtos/ProgresoTestCrearDTO.java
+++ src/main/java/com/example/arteando1/dtos/ProgresoTestCrearDTO.java
@@ -1,4 +1,14 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProgresoTestCrearDTO {
+    private Integer porcentajeCompletado;
+    private Integer usuarioId;
+    private Integer testId;
 }
