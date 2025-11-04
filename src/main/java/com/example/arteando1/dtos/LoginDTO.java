b7f4c641ce25970b91083a3ba55df22ef100d30c
cintia <ccastelgonzalez@safareyes.es>
2025-11-04 16:45:21 +0100

    ✨ Creación de LoginDTO

--- src/main/java/com/example/arteando1/dtos/LoginDTO.java
+++ src/main/java/com/example/arteando1/dtos/LoginDTO.java
@@ -1,4 +1,13 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
+    private String email;
+    private String contrasena;
 }
