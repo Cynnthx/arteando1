225f30bad4d71819b69d5a906dc9f880f4f678c4
cintia <ccastelgonzalez@safareyes.es>
2025-11-04 16:42:37 +0100

    ♻️ Actualización de dto Admin

--- src/main/java/com/example/arteando1/dtos/AdminDTO.java
+++ src/main/java/com/example/arteando1/dtos/AdminDTO.java
@@ -1,4 +1,14 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminDTO {
+    private Integer id;
+    private String nombre;
+    private String email;
 }
