5436c20d4dfefa916119545564c363b6ed634e89
cintia <ccastelgonzalez@safareyes.es>
2025-11-04 16:41:50 +0100

    ♻️ Actualización del modelo de categoria

--- src/main/java/com/example/arteando1/modelos/Categoria.java
+++ src/main/java/com/example/arteando1/modelos/Categoria.java
@@ -1,21 +1,23 @@
 package com.example.arteando1.modelos;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 
 @Entity
-@Table(name = "categorias", schema = "arteando", catalog = "postgres")
+@Table(name = "categorias", schema = "app_arteando")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
-    @Column(name = "nombre", nullable = false, unique = true)
+
+    @Column(name = "nombre", nullable = false, length=50)
     private String nombre;
+
 }
