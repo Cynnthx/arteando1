d859a728f233ddd69921d0107019b92ded88b1bf
cintia <ccastelgonzalez@safareyes.es>
2025-11-04 16:40:31 +0100

    ♻️ Actualización de los dto's de Opcion

--- src/main/java/com/example/arteando1/dtos/OpcionCrearDTO.java
+++ src/main/java/com/example/arteando1/dtos/OpcionCrearDTO.java
@@ -1,4 +1,14 @@
 package com.example.arteando1.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OpcionCrearDTO {
+    private String texto;
+    private Boolean esCorrecta;
+    private Integer preguntaId;
 }
--- src/main/java/com/example/arteando1/dtos/OpcionDTO.java
+++ src/main/java/com/example/arteando1/dtos/OpcionDTO.java
@@ -1,4 +1,28 @@
 package com.example.arteando1.dtos;
 
+import com.example.arteando1.modelos.Opcion;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OpcionDTO {
+    private Integer id;
+    private String texto;
+    private Boolean esCorrecta;
+    private Integer preguntaId;
+    private String preguntaTexto;
+
+    public OpcionDTO(Opcion o) {
+        if (o == null) return;
+        this.id = o.getId();
+        this.texto = o.getTexto();
+        this.esCorrecta = o.getEsCorrecta();
+        if (o.getPregunta() != null) {
+            this.preguntaId = o.getPregunta().getId();
+            this.preguntaTexto = o.getPregunta().getTexto();
+        }
+    }
 }
