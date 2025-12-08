3156bbee8916ea70167497e84a7ba27aa48a7700
cintia <ccastelgonzalez@safareyes.es>
2025-12-08 19:36:14 +0100

    ♻️Actualización de repositorios

--- src/main/java/com/example/arteando1/repositorios/OpcionRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/OpcionRepositorio.java
@@ -4,6 +4,9 @@ import com.example.arteando1.modelos.Opcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OpcionRepositorio extends JpaRepository<Opcion, Integer> {
+    List<Opcion> findByPreguntaId(Integer preguntaId);
 }
--- src/main/java/com/example/arteando1/repositorios/PreguntaRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/PreguntaRepositorio.java
@@ -4,6 +4,11 @@ import com.example.arteando1.modelos.Pregunta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PreguntaRepositorio extends JpaRepository<Pregunta, Integer> {
+    List<Pregunta> findByTestId(Integer testId);
+
+
 }
