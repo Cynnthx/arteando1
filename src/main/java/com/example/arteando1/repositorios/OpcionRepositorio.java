f04665ee1e5972f4f605bd0c5605ef1501fee7bb
cintia <ccastelgonzalez@safareyes.es>
2026-01-18 18:46:54 +0100

    ♻️ Actualización del repositorio de Opcion, faltaba enlazar con Pregunta

--- src/main/java/com/example/arteando1/repositorios/OpcionRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/OpcionRepositorio.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface OpcionRepositorio extends JpaRepository<Opcion, Integer> {
     List<Opcion> findByPreguntaId(Integer preguntaId);
+
+    void deleteByPreguntaId(Integer preguntaId);
 }
