18faad8735d47aa30075b1415e626a1ce4695138
cintia <ccastelgonzalez@safareyes.es>
2026-01-26 19:54:54 +0100

    ♻️ Actualización del repositorio de PuntajeUsuario

--- src/main/java/com/example/arteando1/repositorios/PuntajeUsuarioRepositorio.java
+++ src/main/java/com/example/arteando1/repositorios/PuntajeUsuarioRepositorio.java
@@ -4,6 +4,9 @@ import com.example.arteando1.modelos.PuntajeUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PuntajeUsuarioRepositorio extends JpaRepository<PuntajeUsuario, Integer> {
+    Optional<PuntajeUsuario> findByUsuarioIdAndTestId(Integer usuarioId, Integer testId);
 }
